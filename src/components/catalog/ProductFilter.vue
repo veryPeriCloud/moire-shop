c2f80e3b4f51b979c385ef44cc2a659986749806
Oyuna Zhigzhitzhapova <oyunafront@gmail.com>
2023-07-28 00:01:21 +0600

    feat: productsFilter by price, categoryID, colorID, materialID, seasonID

--- src/components/catalog/ProductFilter.vue
+++ src/components/catalog/ProductFilter.vue
@@ -1,26 +1,88 @@
 <script setup lang="ts">
-import { ref, computed, watch, reactive } from "vue";
+import { ref, computed, watch, reactive, onMounted } from "vue";
+import { API_BASE_URL } from "@/../config.js";
+import axios from "axios";
 
-// const props = defineProps({
-//   priceFrom: {
-//     type: Number,
-//     default: 0,
-//   },
-//   priceTo: {
-//     type: Number,
-//     default: 0,
-//   }
-// });
-
-const formData = reactive({
+type FormData = {
+  priceFrom: Number,
+  priceTo: Number,
+  categoryId: Number,
+  materialIds: Array<Number>,
+  seasonIds: Array<Number>,
+  colorIds: Array<Number>,
+}
+const formData: FormData = reactive({
   priceFrom: 0,
   priceTo: 0,
+  categoryId: 0,
+  materialIds: [],
+  seasonIds: [],
+  colorIds: [],
 })
 
-const emit = defineEmits(['update']);
+const categoriesData = ref(null);
+const materialsData = ref(null);
+const seasonsData = ref(null);
+const colorsData = ref(null);
 
-const submit = () =>{
+const emit = defineEmits(['update', 'reset']);
+
+const fetchCategories = async() => {
+  axios.get(`${API_BASE_URL}/api/productCategories`)
+    .then((response) => { categoriesData.value = response.data; });
+}
+const fetchMaterial = async() => {
+  axios.get(`${API_BASE_URL}/api/materials`)
+    .then((response) => { materialsData.value = response.data; });
+}
+const fetchSeasons = async() => {
+  axios.get(`${API_BASE_URL}/api/seasons`)
+    .then((response) => { seasonsData.value = response.data; });
+}
+const fetchColors = async() => {
+  axios.get(`${API_BASE_URL}/api/colors`)
+    .then((response) => { colorsData.value = response.data; });
+}
+
+onMounted(async()=>{
+  await fetchCategories();
+  await fetchMaterial();
+  await fetchSeasons();
+  await fetchColors();
+})
+
+const categories = computed(() => {
+  return categoriesData.value ? categoriesData.value.items : [];
+})
+const materials = computed(() => {
+  return materialsData.value ? materialsData.value.items : [];
+})
+const seasons = computed(() => {
+  return seasonsData.value ? seasonsData.value.items : [];
+})
+const colors = computed(() => {
+  return colorsData.value ? colorsData.value.items : [];
+})
+
+const submit = () => {
   emit('update', formData);
+}
+const reset = () => {
+  emit('reset', {
+    priceFrom: 0,
+    priceTo: 0,
+    categoryId: 0,
+    materialIds: [],
+    seasonIds: [],
+    colorIds: [],
+  });
+  formData.priceFrom = 0;
+  formData.priceTo = 0;
+  formData.categoryId = 0;
+  formData.materialIds = [];
+  formData.colorIds = [];
+  formData.seasonIds = []
+
 }
 </script>
 
@@ -48,51 +110,55 @@ const submit = () =>{
       <fieldset class="form__block">
         <legend class="form__legend">Категория</legend>
         <label class="form__label form__label--select">
-          <select class="form__select" type="text" name="category">
-            <option value="value1">Все категории</option>
-            <option value="value2">Футболки</option>
-            <option value="value3">Бюстгалтеры</option>
-            <option value="value4">Носки</option>
+          <select class="form__select" name="category" v-model="formData.categoryId">
+            <option value="0">Все категории</option>
+            <option
+              v-for="category in categories"
+              :key="category.id"
+              :value="category.id"
+            >
+              {{ category.title }}
+            </option>
           </select>
         </label>
       </fieldset>
 
       <fieldset class="form__block">
+        <legend class="form__legend">Цвет</legend>
+        <ul class="colors">
+          <li class="colors__item"
+            v-for="color in colors"
+            :key="color.id"
+          >
+            <label class="colors__label">
+              <input class="colors__check sr-only"
+                type="checkbox"
+                name="collection"
+                checked=""
+                :value="color.id"
+                v-model="formData.colorIds"
+              >
+              <span class="colors__value" :style="{ backgroundColor: color.code}"></span>
+            </label>
+          </li>
+        </ul>
+      </fieldset>
+
+      <fieldset class="form__block">
         <legend class="form__legend">Материал</legend>
         <ul class="check-list">          
-          <li class="check-list__item">
+          <li class="check-list__item"
+            v-for="material in materials"
+            :key="material.id"            
+          >
             <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="material" value="лен">
+              <input class="check-list__check sr-only" type="checkbox" name="material"
+                :value="material.id"
+                v-model="formData.materialIds"
+              >
               <span class="check-list__desc">
-                лен 
-                <span>(3)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="material" value="хлопок">
-              <span class="check-list__desc">
-                хлопок 
-                <span>(46)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="material" value="шерсть">
-              <span class="check-list__desc">
-                шерсть 
-                <span>(20)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="material" value="шелк">
-              <span class="check-list__desc">
-                шелк 
-                <span>(30)</span>
+                {{ material.title }} 
+                <span>({{ material.productsCount }})</span>
               </span>
             </label>
           </li>
@@ -102,39 +168,21 @@ const submit = () =>{
       <fieldset class="form__block">
         <legend class="form__legend">Коллекция</legend>
         <ul class="check-list">
-          <li class="check-list__item">
+          <li class="check-list__item"
+            v-for="season in seasons"
+            :key="season.id"
+          >
             <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="collection" value="лето" checked="">
+              <input class="check-list__check sr-only"
+                type="checkbox"
+                name="collection"
+                checked=""
+                :value="season.id"
+                v-model="formData.seasonIds"
+              >
               <span class="check-list__desc">
-                лето 
-                <span>(2)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="collection" value="зима">
-              <span class="check-list__desc">
-                зима 
-                <span>(53)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="collection" value="весна">
-              <span class="check-list__desc">
-                весна 
-                <span>(24)</span>
-              </span>
-            </label>
-          </li>
-          <li class="check-list__item">
-            <label class="check-list__label">
-              <input class="check-list__check sr-only" type="checkbox" name="collection" value="осень">
-              <span class="check-list__desc">
-                осень 
-                <span>(30)</span>
+                {{ season.title }} 
+                <span>({{ season.productsCount }})</span>
               </span>
             </label>
           </li>
@@ -144,9 +192,19 @@ const submit = () =>{
       <button class="filter__submit button button--primery" type="submit">
         Применить
       </button>
-      <button class="filter__reset button button--second" type="button">
+      <button class="filter__reset button button--second" type="button"
+        @click.prevent="reset"
+      >
         Сбросить
       </button>
     </form>
   </aside>
 </template>
+
+<style>
+.colors__label:focus .colors__value::before, 
+.colors__label:hover .colors__value::before, 
+.colors__check:checked~.colors__value::before {
+  border-color: #e02d71;
+}
+</style>
--- src/pages/products/index.vue
+++ src/pages/products/index.vue
@@ -3,6 +3,7 @@ import { reactive, ref, computed, onMounted, watch } from "vue";
 import ProductFilter from "@/components/catalog/ProductFilter.vue";
 import ProductList from "@/components/catalog/ProductList.vue";
 import BasePagination from "@/components/ui/BasePagination.vue";
+import { API_BASE_URL } from "@/../config.js";
 import axios from "axios";
 
 const resolved = ref(false);
@@ -11,11 +12,13 @@ const productsData = ref(null);
 const page = ref(1);
 const productsPerPage = 6;
 
-const filterCategoryId = ref(0);
 const filter = reactive({
   priceFrom: 0,
   priceTo: 0,
   categoryId: 0,
+  materialIds: [],
+  seasonIds: [],
+  colorIds: [],
 })
 
 const products = computed(() => {
@@ -33,17 +36,19 @@ const countOfProducts = computed(() => productsData.value ? productsData.value.p
 
 const fetchProducts = async() => {
   resolved.value = false;
-  await axios.get('https://vue-moire.skillbox.cc/api/products', {
+  await axios.get(`${API_BASE_URL}/api/products`, {
     params: {
       page: page.value,
       limit: productsPerPage,
       minPrice: filter.priceFrom,
       maxPrice: filter.priceTo,
       categoryId: filter.categoryId,
+      materialIds: filter.materialIds,
+      seasonIds: filter.seasonIds,
+      colorIds: filter.colorIds,
     }
   })
     .then((response) => {
-      // console.log(response.data)
       productsData.value = response.data;
     })
     .then(() => resolved.value = true)
@@ -52,16 +57,29 @@ const fetchProducts = async() => {
 const updateFilter = (d) => {
   filter.priceFrom = d.priceFrom;
   filter.priceTo = d.priceTo;
+  filter.categoryId = d.categoryId;
+  filter.materialIds = d.materialIds;
+  filter.seasonIds = d.seasonIds;
+  filter.colorIds = d.colorIds;
 }
 
-onMounted(async ()=> {
+const clearFilter = (d) => {
+  filter.priceFrom = 0;
+  filter.priceTo = 0;
+  filter.categoryId = 0;
+  filter.materialIds = [];
+  filter.seasonIds = [];
+  filter.colorIds = [];
+}
+
+onMounted(async()=> {
   await fetchProducts()
 })
-watch(page, async ()=> {
+watch(page, async()=> {
   await fetchProducts()
 })
 
-watch(filter, async ()=> {
+watch(filter, async()=> {
   await fetchProducts()
 })
 
@@ -83,6 +101,7 @@ watch(filter, async ()=> {
     <div class="content__catalog">
       <ProductFilter 
         @update="updateFilter"
+        @reset="clearFilter"
       />
 
       <section class="catalog" v-if="resolved">
